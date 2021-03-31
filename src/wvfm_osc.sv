a983b4547afc8743d87275fd83f1a1d3ba0891a7
Michelle Chang <mychang@andrew.cmu.edu>
2021-03-31 08:48:56 -0700

    Edited note_en and wave_sel logic

--- src/wvfm_osc.sv
+++ src/wvfm_osc.sv
@@ -9,10 +9,10 @@ module sine
 	   output logic [(ow-1):0]  sin_out );
 
     reg [(ow-1):0] sin_lut [0:((1<<pw)-1)];
-	 initial $readmemh("wvfm_osc/lut/sintable.hex", sin_lut);
+	  initial $readmemh("wvfm_osc/lut/sintable.hex", sin_lut);
 
-	 initial sin_out = 0;
-	 always @(posedge clk)
+	  initial sin_out = 0;
+	  always @(posedge clk)
 	     sin_out <= sin_lut[phase];
 
 endmodule: sine
@@ -24,16 +24,17 @@ module square
      input  wire logic [pw:0] phase,
      output logic [(ow-1):0]  sqr_out );
 
-    logic high;
-    logic [(ow-1):0] high_val;
+     logic high;
+     logic [(ow-1):0] high_val;
 
-    MagComp #21 mag3(.A(phase), .B(21'd500000), .AgtB(high), .AeqB(), .AltB());
+     MagComp #21 mag3(.A(phase), .B(21'd500000), .AgtB(high), .AeqB(), .AltB());
 
-    always @(posedge clk)
-      if (high)
-        sqr_out <= 24'h7fffff;
-      else
-        sqr_out <= 24'h800000;
+     always @(posedge clk) begin
+       if (high)
+         sqr_out <= 24'h7fffff;
+       else
+         sqr_out <= 24'h800000;
+     end
 
 endmodule: square
 
@@ -51,8 +52,7 @@ module triangle
      MagComp #21 mag1(.A(phase), .B(21'd250000), .AgtB(phase2), .AeqB(), .AltB(phase1));
      MagComp #21 mag3(.A(phase), .B(21'd750000), .AgtB(phase4), .AeqB(), .AltB(phase3));
 
-     always @(posedge clk)
-       // phase 1
+     always @(posedge clk) begin
        if (phase1)
          tri_out <= slope * phase;
        else if (phase2)
@@ -61,6 +61,7 @@ module triangle
          tri_out <= (1 << (ow-1)) | (slope * (phase - 21'd500000));
        else if (phase4)
          tri_out <= (1 << (ow-1)) | (slope * (phase - 21'd750000));
+     end
 
 endmodule: triangle
 
@@ -70,83 +71,84 @@ module sawtooth
    ( input wire logic        clk, reset,
      input wire logic [pw:0] phase,
      output logic [(ow-1):0] trg_out );
-	  
-	  logic       neg, phase1;
-	  logic [4:0] slope;
+
+	   logic       neg, phase1;
+	   logic [4:0] slope;
 
      assign slope = 5'd17; // 7fffff / (100000/4)
      MagComp #21 mag1(.A(phase), .B(21'd500000), .AgtB(neg), .AeqB(), .AltB(phase1));
 
-     always @(posedge clk)
+     always @(posedge clk) begin
        if (neg)
          trg_out <= (21'd500000 - phase) * phase;
        else
          trg_out <= (1 << (ow-1)) | ((phase - 21'd500000) * phase);
+     end
 
 endmodule: sawtooth
 
 
 module oscillator
   #( parameter pw = 8, ow = 24 )
-	 ( input  wire logic       clk, reset, en,
-	   input  dsp_to_osc_t     dsp_to_osc,
-	   output reg [(ow-1):0]   out );
+   ( input  wire logic       clk, reset, en,
+     input  wire logic [1:0] wave_sel,
+     input  dsp_to_osc_t     dsp_to_osc,
+     output reg [(ow-1):0]   out );
 
-  logic              clr_phase, load_count, en_d1, en_d2;
-  logic              update_note;
-  logic [10:0]       count;
-  logic [(ow-1):0]   sin_out, sqr_out, saw_out, trg_out;
-	 /* +7 to accomodate for velocity scaling */
-  logic [(ow-1+7):0] wave_out;
-  logic [19:0]       interval;
-  logic [20:0]       phase, max_phase;
-  reg   [20:0]       div_lut [0:87];
+     logic              clr_phase, load_count, note_en_ff, note_en_rise_edge;
+     logic [10:0]       count;
+     logic [(ow-1):0]   sin_out, sqr_out, saw_out, trg_out;
+	   /* +7 to accomodate for velocity scaling */
+     logic [(ow-1+7):0] wave_out;
+     logic [20:0]       interval, phase, max_phase;
+     reg   [20:0]       div_lut [0:87];
 
-	initial begin
-	  clr_phase = 0;
-	  phase = 0;
-	  $readmemh("wvfm_osc/lut/divtable.hex", div_lut);
-	end
-
-	assign max_phase = 1 << 20;
-   //assign update_note = en_d1 & (!en_d2);
-	assign interval = div_lut[dsp_to_osc.note - 7'd21];
-
-	/* Waveform modules will use the incrementing phase values */
-   sine sin(.*);
-   square sqr(.*);
-   triangle trg(.*);
-   sawtooth saw(.*);
-  
-   Counter #11 c(.Q(count), .D(11'd1133), .up(1'd0), .en(1'd1),
-                 .clear(1'd0), .load(load_count), .clk(clk));
-   MagComp #11 mag2(.A(count), .B(1'd0), .AeqB(load_count), .AltB(), .AgtB());
-   Mux4to1 #24 mtx(.sel(dsp_to_osc.wave_sel), .out(wave_out), .*);
-   MagComp #21 mag(.A(phase), .B(max_phase), .AgtB(clr_phase), .AeqB(), .AltB());
-
-   always @(posedge clk) begin
-	  if (reset) begin
-	    out <= 0;
-       phase <= 0;
+     initial begin
+       clr_phase = 0;
+       phase = 0;
+       $readmemh("wvfm_osc/lut/divtable.hex", div_lut);
      end
-     else if (load_count) begin
-       phase <= phase + interval;
-       out <= ((31'b0 | wave_out) * dsp_to_osc.velocity) >> 7;
-       /* update value at sampling frequency */
+
+     assign max_phase = 1 << 20;
+     assign note_en_rise_edge = dsp_to_osc.note_en & (~note_en_ff);
+     assign interval = div_lut[dsp_to_osc.note - 7'd21];
+
+     /* Waveform modules */
+     sine sin(.*);
+     square sqr(.*);
+     triangle trg(.*);
+     sawtooth saw(.*);
+
+     Counter #11 c(.Q(count), .D(11'd1133), .up(1'd0), .en(1'd1),
+                   .clear(1'd0), .load(load_count), .clk(clk));
+     MagComp #11 mag2(.A(count), .B(1'd0), .AeqB(load_count), .AltB(), .AgtB());
+     Mux4to1 #24 mtx(.sel(wave_sel), .out(wave_out), .*);
+     MagComp #21 mag(.A(phase), .B(max_phase), .AgtB(clr_phase), .AeqB(), .AltB());
+
+     /* ff logic to detect rising edge of note_en input */
+     always_ff @(posedge clk or negedge reset) begin
+       if (!reset)
+         note_en_ff <= 1'b0;
+       else
+         note_en_ff <= dsp_to_osc.note_en;
      end
-	  else if (clr_phase)
-	    phase <= 0;
-		 /* reset counter when phase increments over max_phase */
-	end
-	 
-//	always @(posedge clk)
-//	  if (reset) begin
-//	    en_d1 <= 0;
-//		 en_d2 <= 0;
-//     end
-//     else if (dsp_to_osc.note_en) begin
-//       en_d1 <= dsp_to_osc.note_en;
-//       en_d2 <= en_d1;
-//     end
-     
+
+     always @(posedge clk) begin
+       if (reset or note_en_rise_edge) begin
+         out <= 0;
+         phase <= 0;
+         /* when reset signal is on, or when note is first enabled */
+       end
+       else if (load_count) begin
+         phase <= phase + interval;
+         out <= ((31'b0 | wave_out) * dsp_to_osc.velocity) >> 7;
+         /* update value at sampling frequency */
+       end
+       else if (clr_phase) begin
+         phase <= 0;
+         out <= out;
+         /* reset counter when phase increments over max_phase */
+       end
+     end
+
 endmodule: oscillator
